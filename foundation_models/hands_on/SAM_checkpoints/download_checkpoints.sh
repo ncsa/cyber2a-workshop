dc54b9c2c9ccdd2c674299ffed077e0ab8b502e9
Minu Mathew <minumpmathews@gmail.com>
2024-10-22 17:46:13 -0500

    having SAM checkpoints in dockerfile download

--- foundation_models/hands_on/SAM_checkpoints/download_checkpoints.sh
+++ foundation_models/hands_on/SAM_checkpoints/download_checkpoints.sh
@@ -7,9 +7,9 @@ sam2_hiera_s_url="${BASE_URL}/sam2.1_hiera_small.pt"
 sam2_hiera_b_plus_url="${BASE_URL}/sam2.1_hiera_base_plus.pt"
 sam2_hiera_l_url="${BASE_URL}/sam2.1_hiera_large.pt"
 
-wget -nc -O sam2.1_hiera_tiny.pt $sam2_hiera_t_url
-wget -nc -O sam2.1_hiera_small.pt $sam2_hiera_s_url
-wget -nc -O sam2.1_hiera_base_plus.pt $sam2_hiera_b_plus_url
-wget -nc -O sam2.1_hiera_large.pt $sam2_hiera_l_url
+# wget -nc -O ./foundation_models/SAM_checkpoints/sam2.1_hiera_tiny.pt $sam2_hiera_t_url
+# wget -nc -O ./foundation_models/SAM_checkpoints/sam2.1_hiera_small.pt $sam2_hiera_s_url
+# wget -nc -O ./foundation_models/SAM_checkpoints/sam2.1_hiera_base_plus.pt $sam2_hiera_b_plus_url
+wget -nc -O ./foundation_models/SAM_checkpoints/sam2.1_hiera_large.pt $sam2_hiera_l_url
 
 echo "Downloaded all SAM-2 checkpoints successfully."
